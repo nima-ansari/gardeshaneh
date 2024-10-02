1d5368ea91edce455cc514075f171f679c94fd35
nima-ansari <nimaansariwork@gmail.com>
2024-10-02 14:32:23 +0330

    home page - added

--- Gardeshaneh/Gardeshaneh/wwwroot/js/site.js
+++ Gardeshaneh/Gardeshaneh/wwwroot/js/site.js
@@ -1,4 +1,46 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function ShowMessage(title, text, theme) {
+    window.createNotification({
+        closeOnClick: true,
+        displayCloseButton: false,
+        positionClass: 'nfc-bottom-right',
+        showDuration: 7000,
+        theme: theme !== '' ? theme : 'success'
+    })({
+        title: title !== '' ? title : 'پیام',
+        message: decodeURI(text)
+    });
+}
 
-// Write your JavaScript code.
+function goBack() {
+    window.history.go(-1);
+}
+
+function filterForm(pageId) {
+    $("#PageId").val(pageId);
+    $("#filter-form").submit();
+}
+
+function filterFormOfFirstPage() {
+    $("#PageId").val(1);
+    $("#filter-form").submit();
+}
+
+function copyPlaceLinkToClipboard() {
+    let input = document.getElementById("place-url");
+    let title = input.value;
+    input.select();
+    input.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    setTimeout(() => {
+        input.value = "کپی شد";
+        input.classList.add("rtl");
+        input.classList.add("text-center");
+        input.style.boxShadow = "0 0 0 0.25rem rgb(9 156 7 / 25%)";
+    }, 100);
+    setTimeout(() => {
+        input.value = title;
+        input.classList.remove("rtl");
+        input.classList.remove("text-center");
+        input.style.boxShadow = "0 0 0 0 rgb(9 156 7 / 25%)";
+    }, 3000);
+}
